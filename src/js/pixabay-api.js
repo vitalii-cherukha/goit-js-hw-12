6688413009593bbd4dbe3dcc2b4598d13865fbb3
vitalii <vitalii.cherukha@gmail.com>
2025-07-22 08:52:30 +0300

    all fix

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
-export const getImagesByQuery = (query, page) => {
-  return axios
-    .get(`https://pixabay.com/api/`, {
-      params: {
-        key: '51362773-b845efdff4000eb7d694ec90c',
-        q: query,
-        page: page,
-        per_page: '15',
-        orientation: 'horizontal',
-        safesearch: 'true',
-        image_type: 'photo',
-      },
-    })
-    .then(response => response.data);
+export const getImagesByQuery = async (query, page) => {
+  const response = await axios.get(`https://pixabay.com/api/`, {
+    params: {
+      key: '51362773-b845efdff4000eb7d694ec90c',
+      q: query,
+      page: page,
+      per_page: 15,
+      orientation: 'horizontal',
+      safesearch: true,
+      image_type: 'photo',
+    },
+  });
+  return response.data;
 };
